8ca8a0cd6085c76a1d39928a58c199bf627107c0
KASSY <133909539+DollyWhite06@users.noreply.github.com>
2025-02-13 16:13:51 -0600

    Update app.routes.ts

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,13 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { ContactoComponent } from './contacto/contacto.component';
+import { SolarisComponent } from './solaris/solaris.component';
+import { ProductosComponent } from './productos/productos.component';
+export const routes: Routes = [
+{path: 'home', component: HomeComponent},
+{path: 'contacto', component: ContactoComponent},
+{path: 'solaris', component: SolarisComponent},
+{path: 'productos', component: ProductosComponent}
 
-export const routes: Routes = [];
+
+];
